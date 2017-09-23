fa76f7f5c78f2d481863241a77de6877a76b344b
unknown <ankit.kumar2788@gmail.com>
2017-09-23 22:19:05 +0530

    files changed

---
+++ Bank/src/Bank/Connection-copy.java
@@ -0,0 +1,24 @@
+package Bank;
+
+import java.sql.PreparedStatement;
+
+public class Connection {
+	
+	public static void getEmp(){
+		
+		Connection con = null;
+		PreparedStatement mst = null;
+		String sqlQry ="select * from emp";
+		
+		
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- Bank/src/Bank/Sunil.java
+++ Bank/src/Bank/Sunil.java
@@ -7,6 +7,9 @@ public class Sunil {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		
+		GGGGGGGGGGGGGGGGGGGGGGGGGGG
 
 	}
 
